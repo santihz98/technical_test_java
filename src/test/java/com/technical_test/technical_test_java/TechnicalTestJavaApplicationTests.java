5c8e485c8baf41f396a02166d7700fcc2e06aa84
santihz98 <sahz1998@gmail.com>
2024-08-17 20:55:39 -0500

    Se crea el proyecto

---
+++ src/test/java/com/technical_test/technical_test_java/TechnicalTestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical_test.technical_test_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
